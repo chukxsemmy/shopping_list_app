2f5dc300b4291199003a385f6d28e83d7495349f
Emma-soft <Muonaemmanuel044@gmail.com>
2023-12-19 12:49:17 +0100

    sending error messages to user

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -18,6 +18,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryitems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -29,6 +30,11 @@ class _GroceryListState extends State<GroceryList> {
     final url = Uri.https(
         'flutter-prep-afb82-default-rtdb.firebaseio.com', 'shoping-list.json');
     final response = await http.get(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Please try again later.';
+      });
+    }
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final items in listData.entries) {
@@ -101,6 +107,9 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ),
       );
+    }
+    if (_error != null) {
+      content = Center(child: Text(_error!));
     }
     return Scaffold(
       appBar: AppBar(
